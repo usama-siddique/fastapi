8b32040e817e146dd6ddd623fcd8898d2a8c6945
Muhammad Usama Siddique <63277069+usama-siddique@users.noreply.github.com>
2025-06-01 20:18:19 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,4 +4,24 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI on Vercel!"}
+    data = {
+        "Clue Carre - Downtown": {
+            "Sun": 32,
+            "Mon": 28,
+            "Tue": 28,
+            "Wed": 28,
+            "Thu": 28,
+            "Fri": 32,
+            "Sat": 32
+        },
+        "Clue Carre - Metairie": {
+            "Sun": 24,
+            "Mon": 21,
+            "Tue": 21,
+            "Wed": 21,
+            "Thu": 21,
+            "Fri": 24,
+            "Sat": 24
+        }
+    }
+    return data
