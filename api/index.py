901cf30989fc36bd6128512b5d3d35ec15bf1367
Muhammad Usama Siddique <63277069+usama-siddique@users.noreply.github.com>
2025-06-30 12:38:22 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,19 +6,19 @@ app = FastAPI()
 def read_root():
     data = {
         "Clue Carre - Downtown": {            
-            "Mon": 28,
+            "Mon": 23,
             "Tue": 27,
             "Wed": 28,
             "Thu": 28,
             "Fri": 31,
             "Sat": 30,
-            "Sun": 23
+            "Sun": 31
         },
         "Clue Carre - Metairie": {            
             "Mon": 21,
             "Tue": 21,
-            "Wed": 21,
-            "Thu": 20,
+            "Wed": 9,
+            "Thu": 19,
             "Fri": 24,
             "Sat": 23,
             "Sun": 9
