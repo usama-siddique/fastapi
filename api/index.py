007b8bc40777e35d8d4b72cde89c1f5e886cfe8a
Muhammad Usama Siddique <63277069+usama-siddique@users.noreply.github.com>
2025-07-06 17:31:55 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,19 +8,19 @@ def read_root():
         "Clue Carre - Downtown": {            
             "Mon": 25,
             "Tue": 27,
-            "Wed": 28,
+            "Wed": 27,
             "Thu": 28,
-            "Fri": 30,
-            "Sat": 27,
-            "Sun": 30
+            "Fri": 31,
+            "Sat": 30,
+            "Sun": 25
         },
         "Clue Carre - Metairie": {            
             "Mon": 21,
             "Tue": 21,
             "Wed": 20,
             "Thu": 21,
-            "Fri": 15,
-            "Sat": 19,
+            "Fri": 23,
+            "Sat": 20,
             "Sun": 19
         }
     }
